602814a551442ab14571bc761ab24ad6de9ccdbf
DSubhadipSahoo <subhadip.sahoo@tier5.in>
2022-10-21 23:04:39 +0530

    Updated HTML and spec file

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code id="appID">src/App.js</code> and save to reload.
+          Edit <code id="appID">src/AppFailed.js</code> and save to reload.
         </p>
         <a
           className="App-link"
